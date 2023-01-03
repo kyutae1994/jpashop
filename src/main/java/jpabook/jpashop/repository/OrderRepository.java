235a1d2e36ddf9e22fa0d48bacff729ce21c97d3
kyutae1994 <k305059ute@gmail.com>
2023-01-03 22:22:31 +0900

    엔티티를 dto로 변환 fetch 조인 공부

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -93,4 +93,13 @@ public class OrderRepository {
         TypedQuery<Order> query = em.createQuery(cq).setMaxResults(1000);
         return query.getResultList();
     }
+
+    // fetch 조인
+    public List<Order> findAllWithMemberDelivery() {
+        return em.createQuery(
+                "select o from Order o" +
+                        " join fetch o.member m" +
+                        " join fetch o.delivery d", Order.class
+        ).getResultList();
+    }
 }
