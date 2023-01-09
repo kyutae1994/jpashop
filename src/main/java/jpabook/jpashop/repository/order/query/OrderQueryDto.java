72957b5b377a41766c40f6183332ab09559bd36b
kyutae1994 <k305059ute@gmail.com>
2023-01-09 22:49:35 +0900

    한번에 조회 공부

--- src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
@@ -17,12 +17,20 @@ public class OrderQueryDto {
     private Address address;
     private List<OrderItemQueryDto> orderItems;
 
-    public OrderQueryDto(Long orderId, String name, LocalDateTime localDateTime, OrderStatus orderStatus, Address address/*, List<OrderItemQueryDto> orderItems*/) {
+    public OrderQueryDto(Long orderId, String name, LocalDateTime localDateTime, OrderStatus orderStatus, Address address) {
         this.orderId = orderId;
         this.name = name;
         this.localDateTime = localDateTime;
         this.orderStatus = orderStatus;
         this.address = address;
-//        this.orderItems = orderItems;
+    }
+
+    public OrderQueryDto(Long orderId, String name, LocalDateTime localDateTime, OrderStatus orderStatus, Address address, List<OrderItemQueryDto> orderItems) {
+        this.orderId = orderId;
+        this.name = name;
+        this.localDateTime = localDateTime;
+        this.orderStatus = orderStatus;
+        this.address = address;
+        this.orderItems = orderItems;
     }
 }
