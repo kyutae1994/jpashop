bb7264fef14c7f443b19403485a5ad2ce810c262
kyutae1994 <k305059ute@gmail.com>
2023-01-09 22:19:51 +0900

    JPA에서 데이터 직접 조회 공부

---
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
@@ -0,0 +1,28 @@
+package jpabook.jpashop.repository.order.query;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.OrderStatus;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+public class OrderQueryDto {
+
+    private Long orderId;
+    private String name;
+    private LocalDateTime localDateTime;
+    private OrderStatus orderStatus;
+    private Address address;
+    private List<OrderItemQueryDto> orderItems;
+
+    public OrderQueryDto(Long orderId, String name, LocalDateTime localDateTime, OrderStatus orderStatus, Address address/*, List<OrderItemQueryDto> orderItems*/) {
+        this.orderId = orderId;
+        this.name = name;
+        this.localDateTime = localDateTime;
+        this.orderStatus = orderStatus;
+        this.address = address;
+//        this.orderItems = orderItems;
+    }
+}
