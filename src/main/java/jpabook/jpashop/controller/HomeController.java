aee7d8a9653c4e4897bc42a390ff84e4fcb6f2ee
ktkim <ktkim94@becomesolution.com>
2023-02-14 16:30:04 +0900

    로그인 구현

--- src/main/java/jpabook/jpashop/controller/HomeController.java
+++ src/main/java/jpabook/jpashop/controller/HomeController.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.controller;
 
 import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.session.SessionConst;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
---
+++ src/main/java/jpabook/jpashop/session/SessionConst.java
@@ -0,0 +1,5 @@
+package jpabook.jpashop.session;
+
+public interface SessionConst {
+    String LOGIN_MEMBER = "loginMember";
+}
