4ce7d7c1e9a0792a3d69e5044feb26e8e63158cb
kyutae1994 <k305059ute@gmail.com>
2023-02-14 23:11:03 +0900

    로그인 구현

--- src/main/java/jpabook/jpashop/controller/MemberForm.java
+++ src/main/java/jpabook/jpashop/controller/MemberForm.java
@@ -8,9 +8,13 @@ import javax.validation.constraints.NotEmpty;
 @Getter @Setter
 public class MemberForm {
 
-    @NotEmpty(message = "회원 이름은 필수 입니다") // 값이 없으면 오류
-    private String name;
+    @NotEmpty(message = "이메일은 필수 입니다") // 값이 없으면 오류
+    private String email;
 
+    @NotEmpty(message = "비밀번호는 필수 입니다") // 값이 없으면 오류
+    private String password;
+
+    private String name;
     private String city;
     private String street;
     private String zipcode;
---
+++ src/main/java/jpabook/jpashop/sessioin/SessionConst.java
@@ -0,0 +1,5 @@
+package jpabook.jpashop.sessioin;
+
+public interface SessionConst {
+    String LOGIN_MEMBER = "loginMember";
+}
