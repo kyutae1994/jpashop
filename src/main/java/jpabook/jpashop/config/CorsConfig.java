037a4a2d6848e4d5f0698b3a200ffb1f09a67c2e
kyutae1994 <k305059ute@gmail.com>
2023-03-19 23:06:23 +0900

    로그인 jwt 토큰 구현

--- src/main/java/jpabook/jpashop/config/CorsConfig.java
+++ src/main/java/jpabook/jpashop/config/CorsConfig.java
@@ -17,8 +17,7 @@ public class CorsConfig {
         config.addAllowedOrigin("*"); // 모든 ip에 응답을 허용
         config.addAllowedHeader("*"); // 모든 header에 응답을 허용
         config.addAllowedMethod("*"); // 모든 post, get, put, delete, patch 요청을 허용
-        source.registerCorsConfiguration("/admin/**", config);
-        source.registerCorsConfiguration("/user/**", config);
+        source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
     }
 }
